0432d7f96230df3d4a1e654da4d53f7def4796ac
Huan.Wang <freddie218@163.com>
2014-02-27 23:14:34 +0800

    SUX-24, Starting to shift the fundamential structure of the story line ---- should always create a event before setting expense. huan

---
+++ chyy/EventViewController.h
@@ -0,0 +1,15 @@
+//
+//  EventViewController.h
+//  chyy
+//
+//  Created by huan on 2/27/14.
+//  Copyright (c) 2014 huan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventViewController : UITableViewController
+
+@property (strong) NSMutableArray *events;
+
+@end
