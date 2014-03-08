a675d1ee59f1bd27024d83027656a00cf49aa761
Huan.Wang <freddie218@163.com>
2014-03-08 23:04:57 +0800

    SUX-28, can add a new category. huan

---
+++ chyy/CategoryDetailViewController.h
@@ -0,0 +1,23 @@
+//
+//  CategoryDetailViewController.h
+//  chyy
+//
+//  Created by huan on 3/8/14.
+//  Copyright (c) 2014 huan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Category.h"
+
+@interface CategoryDetailViewController : UIViewController <UITextFieldDelegate>
+
+@property (strong) Category *category;
+
+@property (strong, nonatomic) IBOutlet UITextField *nameTextField;
+
+@property (strong, nonatomic) IBOutlet UITextField *currentTextField;
+
+- (IBAction)cancel:(id)sender;
+- (IBAction)save:(id)sender;
+
+@end
---
+++ chyy/CategoryDetailViewController.m
@@ -0,0 +1,123 @@
+//
+//  CategoryDetailViewController.m
+//  chyy
+//
+//  Created by huan on 3/8/14.
+//  Copyright (c) 2014 huan. All rights reserved.
+//
+
+#import "CategoryDetailViewController.h"
+
+@implementation CategoryDetailViewController
+
+@synthesize category;
+
+- (NSManagedObjectContext *) managedObjectContext
+{
+    NSManagedObjectContext *context = nil;
+    id delegate = [[UIApplication sharedApplication] delegate];
+    
+    if ([delegate performSelector:@selector(managedObjectContext)]) {
+        context = [delegate managedObjectContext];
+    }
+    
+    return context;
+}
+
+- (IBAction)cancel:(id)sender
+{
+    self.currentTextField.text = @"cancelEditing";
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)save:(id)sender
+{
+    
+    if(![[self.nameTextField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] length]) {
+        //string is all whitespace
+        UIAlertView *av = [[UIAlertView alloc] initWithTitle:@"Save Error"
+                                                     message:@"Can not be empty!"
+                                                    delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [av show];
+        return;
+    }
+    
+    NSManagedObjectContext *context = [self managedObjectContext];
+    
+    if (self.category) {
+        category.name = self.nameTextField.text;
+    } else {
+        Category *newCategory = [NSEntityDescription insertNewObjectForEntityForName:@"Category" inManagedObjectContext:context];
+        
+        newCategory.name = self.nameTextField.text;
+        
+    }
+    
+    NSError *error = nil;
+    if (![context save:&error]) {
+        NSLog(@"can't save! %@ %@", error, [error localizedDescription]);
+    }
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+}
+
+- (BOOL)textFieldShouldEndEditing:(UITextField *)textField {
+    if (textField == self.nameTextField) {
+        if(![[textField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] length]) {
+            //string is all whitespace
+            UIAlertView *av = [[UIAlertView alloc] initWithTitle:@"Entry Error"
+                                                         message:@"Can not be empty!"
+                                                        delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [av show];
+            return NO;
+        }
+    }
+    return YES;
+}
+
+- (void) dismissKeyboard
+{
+    [self.nameTextField resignFirstResponder];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    self.currentTextField = textField;
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard)];
+    [self.view addGestureRecognizer:tap];
+    
+    if (self.category) {
+        [self.nameTextField setText:category.name];
+    }
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
